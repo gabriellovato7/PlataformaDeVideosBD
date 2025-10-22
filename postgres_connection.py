0607ca7d93eab45eeef4abf17da42263aa0c43cd
GabrielLovato <gabriellcc20@gmail.com>
2025-10-21 21:54:08 -0300

    feat: postgres_connection.py

---
+++ postgres_connection.py
@@ -0,0 +1,19 @@
+import psycopg2
+from psycopg2 import sql
+
+def connect_postgres():
+    try:
+        connection = psycopg2.connect(
+            host="db.wqyjmvwsippsgndjlvpk.supabase.co",   
+            port="5432",
+            database="postgres",          
+            user="postgres",              
+            password="Noronhja2004"      
+        )
+
+        print("Conexao realizada")
+        return connection
+
+    except Exception as e:
+        print("Erro:", e)
+        return None
